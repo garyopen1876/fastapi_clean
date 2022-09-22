f4dea869a74918fc5fae1416f35a4ea9341589d5
Garyopen <garyopen1876@gmail.com>
2022-09-22 16:23:58 +0800

    user.py add Body and User CRUD

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,11 +1,10 @@
-from typing import Union
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Body
 
 router = APIRouter()
 
 
 @router.post("/login", summary="user login")
-def read_item(username: str, password: str):
+def user_login(username: str = Body(...), password: str = Body(...)):
     try:
         return {"login_list": "success"}
     except Exception as e:
@@ -13,3 +12,26 @@ def read_item(username: str, password: str):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Operation failed. {e}"
         )
+
+
+@router.post("/", summary="user create")
+def user_create(username: str = Body(...), password: str = Body(...), email: str = Body(...)):
+    try:
+        return {"user_create_list": "success"}
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Operation failed. {e}"
+        )
+
+
+@router.delete("/", summary="user delete")
+def user_delete(user_id: int):
+    try:
+        return {"user_delete_list": "success"}
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Operation failed. {e}"
+        )
+
