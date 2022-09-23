74b2faa1538bacf95edf70abfb7441ef2530d391
Garyopen <garyopen1876@gmail.com>
2022-09-23 10:02:13 +0800

    create sqlalchemy and alembic

---
+++ app/db/db_connect.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv("./.env")
+SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL")
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
+    echo=False,
+    pool_size=100
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
