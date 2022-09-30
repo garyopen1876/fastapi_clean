ea9a8b9d8fd62fbc506fa07439a021f011d69c80
Garyopen <garyopen1876@gmail.com>
2022-09-30 09:15:55 +0800

    fix login path

--- app/routers/login.py
+++ app/routers/login.py
@@ -5,6 +5,6 @@ from app.utils import login as utils
 router = APIRouter()
 
 
-@router.post("/login", status_code=status.HTTP_200_OK, summary="Login")
+@router.post("/", status_code=status.HTTP_200_OK, summary="Login")
 def login(username: str = Body(...), password: str = Body(...)):
     return utils.login(username, password)
