e23fa4b604490225ce162359e89dc22e5670e43c
Garyopen <garyopen1876@gmail.com>
2022-09-30 16:26:17 +0800

    add login schemas

--- app/utils/login.py
+++ app/utils/login.py
@@ -1,5 +1,6 @@
 from app.crud.crud_users import CRUDUser
 from app.setting import settings
+from app.schemas.login import LoginMessage
 from datetime import datetime, timedelta
 from fastapi import HTTPException, status
 from jose import jwt
@@ -7,14 +8,14 @@ from jose import jwt
 import bcrypt
 
 
-def login(username: str, password: str):
+def login(username: str, password: str) -> LoginMessage:
     db = CRUDUser()
 
     user_password = db.get_user_password(username)
     if user_password and bcrypt.checkpw(password.encode('utf8'), user_password[0][0].encode('utf8')):
         token = jwt.encode({"username": username, "exp": datetime.utcnow() + timedelta(minutes=settings.token_limit_minutes)},
                            settings.jwt_secret, algorithm="HS256")
-        return {"message": "Login successfully", "token": token}
+        return LoginMessage(**{"message": "Login successfully", "token": token})
 
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
