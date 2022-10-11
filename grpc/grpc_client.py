cf9e654c36c5a22b8ca3530985614cf00c707654
Garyopen <garyopen1876@gmail.com>
2022-10-11 10:10:49 +0800

    change commit

--- grpc/grpc_client.py
+++ grpc/grpc_client.py
@@ -4,11 +4,11 @@ import grpc_pb2, grpc_pb2_grpc
 
 
 def run():
-    # 本次不使用SSL，所以channel是不安全的
+    # grpc connect
     channel = grpc.insecure_channel('localhost:50054')
-    # 客户端实例
     stub = grpc_pb2_grpc.GreeterStub(channel)
-    # 调用服务端方法
+    
+    # 呼叫 server service
     response = stub.SayHello(grpc_pb2.HelloRequest(name='World'))
     print("Greeter client received: " + response.message)
 
