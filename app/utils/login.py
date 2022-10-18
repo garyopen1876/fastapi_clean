da727b4aba096e2dc2637b99ad64ea05e0949ef2
Garyopen <garyopen1876@gmail.com>
2022-10-18 08:28:54 +0800

    change login function to gRPC

--- app/utils/login.py
+++ app/utils/login.py
@@ -1,8 +1,7 @@
 from app.schemas.login import LoginMessage
 from app.utils.app_error import UsernamePasswordError
-from app.utils import grpc_pb2_grpc
-from app.utils import grpc_pb2
-
+from app.grpc import grpc_pb2_grpc
+from app.grpc import grpc_pb2
 import grpc
 
 
@@ -10,9 +9,9 @@ def login(username: str, password: str) -> LoginMessage:
     # grpc connect
     channel = grpc.insecure_channel('localhost:50054')
     stub = grpc_pb2_grpc.LoginStub(channel)
-
     # 呼叫 server service
     response = stub.Login(grpc_pb2.LoginRequest(
         username=username, password=password))
-
-    return {"response": response}
+    if response.message == "Login Error":
+        raise UsernamePasswordError()
+    return {"message": response.message}
