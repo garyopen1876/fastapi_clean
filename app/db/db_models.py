2c1549229b05ac86e5c83a81330395eaa477ed4a
Garyopen <garyopen1876@gmail.com>
2023-03-03 08:08:25 +0800

    db schemas append relationship

--- app/db/db_models.py
+++ app/db/db_models.py
@@ -1,13 +1,26 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Sequence
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Sequence, DateTime
 from sqlalchemy.orm import relationship
 from .db_connect import Base
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
 
     id = Column(Integer, Sequence("user_id_seq"), primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     password = Column(String)
     email = Column(String, unique=True, index=True)
     is_active = Column(Boolean, default=False)
+    # one to many
+    message_owner = relationship("Message", back_populates="user_id")
+
+
+class Message(Base):
+    __tablename__ = "message"
+
+    id = Column(Integer, Sequence("message_id_seq"), primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey(User.id))
+    title = Column(String, index=True)
+    content = Column(String, nullable=True)
+    create_time = Column(DateTime)
+    update_time = Column(DateTime)
